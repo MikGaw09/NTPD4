08ef78a6a7902d0d9b36b0dc93cfd62ed96c1046
gostek223 <pingwin.gawronek@gmail.com>
2026-04-16 01:23:46 +0200

    added app and model

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
+from symtable import Class
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+import pickle
+import numpy as np
+import sklearn
 
+with open('C:/Users/mikga/PycharmProjects/JupyterProject2/model_iris_v1.pkl', 'rb') as f:
+        model = pickle.load(f)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+class Item(BaseModel):
+    sl: float
+    sw: float
+    pl: float
+    pw: float
+@app.post("/predict")
+async def predict(item : Item):
+    predict=model.predict([[item.sl,item.sw,item.pl,item.pw]])
+    prediction = int(predict[0])
+    return {"predict": prediction}
